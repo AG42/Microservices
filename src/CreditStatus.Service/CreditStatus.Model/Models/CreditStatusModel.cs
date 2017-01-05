5529940a71f40e7914859d776f2d6a0c689e7c70
csing104 <shashanksingh59@yahoo.co.in>
2017-01-05 13:00:40 +0530

    Removing files
    Removing files

--- src/CreditStatus.Service/CreditStatus.Model/Models/CreditStatusModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CreditStatus.Model.Models
-{
-   public  class CreditStatusModel
-    {
-        public string CustomerCode { get; set; }
-        public string CustomerName { get; set; }
-        public bool Status { get; set; }
-    }
-}
--- src/CreditStatus.Service/CreditStatus.Model/Response/CreditResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using CreditStatus.Model.Models;
-
-namespace CreditStatus.Model.Response
-{
-   public  class CreditResponse : BaseResponse
-    {
-        public CreditStatusModel Credit { get; set; }
-    }
-}
