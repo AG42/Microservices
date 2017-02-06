65318535162db1adca975a4912165d2482ff3b49
csing103 <csing103@jci.com>
2017-02-06 12:04:35 +0530

    Deleted TaxInvoice

--- src/TaxInvoice.Service/TaxInvoice.Common/Error/ErrorMessageInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-namespace TaxInvoice.Common.Error
-{
-    public class ErrorMessageInfo
-    {
-        public string Message { get; set; }
-        public HttpStatusCode StatusCode { get; set; }
-    }
-}
